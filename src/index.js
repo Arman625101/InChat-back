e79cad4e2563540179a936fb2be14eaba6fc31bf
unknown <vip.arman2011@gmail.com>
2018-05-15 01:40:45 +0400

    scoket io testing new user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
+});
+
+let users = [];
+io.on('connection', (socket) => {
+  let addedUser = false;
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+    addedUser = true;
+    users = [...users, { username }];
+  });
+  socket.emit('users', users);
 });
 
 http.listen(port, () => {
