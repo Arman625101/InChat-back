4c3d6af6e932e034390d0413481ba45f7856a3ab
unknown <vip.arman2011@gmail.com>
2018-05-19 17:54:26 +0400

    handle disconect event to delete client

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ app.get('/', (req, res) => {
 let users = [];
 io.on('connection', (socket) => {
   let addedUser = false;
-  socket.on('add_user', (user) => {
+  socket.on('add_user', (name) => {
     if (addedUser) return;
     addedUser = true;
-    users = [...users, user];
+    users = [...users, { name, id: socket.id }];
     io.emit('get_users', users);
   });
   socket.on('send_message', data => io.emit('get_messages', data));
+  socket.on('disconnect', () => {
+    users = users.filter(user => user.id !== socket.id);
+    io.emit('get_users', users);
+  });
 });
 
 http.listen(port, () => {
