9b6d5a8a74bf3e87ffa675ccc3ea63caa46d5174
unknown <vip.arman2011@gmail.com>
2018-05-20 02:54:10 +0400

    small fixes

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+// app.get('/', (req, res) => {
+//   res.send('');
+// });
 
 let users = [];
+
 io.on('connection', (socket) => {
   let addedUser = false;
+  // New user
   socket.on('add_user', (name) => {
     if (addedUser) return;
     addedUser = true;
     users = [...users, { name, id: socket.id }];
     io.emit('get_users', users);
   });
+  // New message
   socket.on('send_message', data => io.emit('get_messages', data));
+  // Disconnect
   socket.on('disconnect', () => {
     users = users.filter(user => user.id !== socket.id);
     io.emit('get_users', users);
