f5eb6198888f298907211dff0f14e64ca3c937b8
unknown <vip.arman2011@gmail.com>
2018-05-20 18:51:04 +0400

    mongodb connected user collection added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+require('dotenv').config();
 
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}:29540/inchat`);
+
+const db = mongoose.connection;
+const User = require('./models/User');
+
+db.once('open', () => {
+  console.log('succesfull');
+});
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('');
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send());
+
+app.post('/new-user', (req, res) => {
+  const {
+    email, username, password, passwordConf,
+  } = req.body;
+  if (email && username && password && passwordConf) {
+    const userData = {
+      email,
+      username,
+      password,
+      passwordConf,
+    };
+
+    User.create(userData, (err, data) => console.log(data));
+  }
+});
 
 let users = [];
 
---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre('save', () => {
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    this.password = hash;
+  });
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
